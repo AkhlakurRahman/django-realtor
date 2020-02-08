8c52313f148b7b1f8e9afd4602138270d13d558b
AkhlakurRahman <akhlakur.rahmann@gmail.com>
2020-02-08 18:23:06 +0600

    admin panel customised

--- listings/admin.py
+++ listings/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
 
+from .models import Listing
 # Register your models here.
+
+
+class ListingAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'is_published',
+                    'price', 'list_date', 'seller')
+
+    list_display_links = ('id', 'title')
+
+    list_filter = ('seller',)
+
+    list_editable = ('is_published',)
+
+    search_fields = ('title', 'description', 'address',
+                     'city', 'state', 'zipcode')
+
+    list_per_page = 25
+
+
+admin.site.register(Listing, ListingAdmin)
