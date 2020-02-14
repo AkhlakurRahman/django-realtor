a7f3c6ad99444cc66a87104236a44e957819cec3
AkhlakurRahman <akhlakur.rahmann@gmail.com>
2020-02-14 18:08:51 +0600

    contact form backend logic added

--- realtor/settings.py
+++ realtor/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = [
     'listings.apps.ListingsConfig',
     'sellers.apps.SellersConfig',
     'accounts.apps.AccountsConfig',
+    'contact.apps.ContactConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
